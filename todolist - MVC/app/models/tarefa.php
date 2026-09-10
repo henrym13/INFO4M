5efeaf2a55be4441104a6a4a1ee39bd3c020f440
HENRY MIGUEL OLIVEIRA BARRETO DE LIMA <20231134010013@ifrn.local>
2026-09-10 10:28:36 -0300

    subindo novo projeto

---
+++ todolist - MVC/app/models/tarefa.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ ."/../config/database.php";
+
+class Tarefas{
+    private $conn;
+    public function __construct(){
+        $db = new Database();
+        $this->conn = $db->conectar();
+
+    }   
+};
+
+
+
+
+
+
+
+
+
+
+
+
+?>
